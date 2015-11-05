765bb015ea9dbceac9b0e49677f504c54ee1b396
JVThomas <jthomas0209@gmail.com>
2015-11-05 12:22:23 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize(input)
+    @ceiling = input
+    @arrNum = []
+  end
+
+  def lcm
+    num = 0
+    value_found = false
+    
+    for i in 1..@ceiling
+      @arrNum.push(i)
+    end
+
+    while value_found == false
+      num += 1
+      value_found = @arrNum.all? do |element|
+        num % element == 0
+      end
+    end
+    num
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def smallest_multiple(input)
+  arrNum = []
+  value_found = false
+  num = 0
+  
+  for i in 1..input
+    arrNum.push(i)
+  end
+
+  while value_found == false
+    num += 1
+    value_found = arrNum.all? do |element|
+      num % element == 0
+    end
+  end
+  num
+end
